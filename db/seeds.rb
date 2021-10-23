14f726450de759fe952134a82ca3d6215694eb91
laura <l.keeley000@gmail.com>
2021-10-23 14:46:27 -0500

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,75 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# User.create!([
+#   {
+#     name: "Jack",
+#     email: "jack@gmail.com",
+#     password: "password",
+#     password_confirmation: "password",
+#     location: "West Side",
+#     image_url: "https://unsplash.com/photos/mDiFpFl_jTs"
+#   },
+#   {
+#     name: "Ruby",
+#     email: "ruby@gmail.com",
+#     password: "password",
+#     password_confirmation: "password",
+#     location: "East Side",
+#     image_url: "https://unsplash.com/photos/CgWTqYxHEkg"
+#   },
+  
+# ])
+
+# Category.create!([
+#   {
+#     name: "Bees"
+#   },
+#   {
+#     name: "Perennial Flowers",
+#   },
+  
+# ])
+
+# Post.create!([
+#   {
+#     title: "Beekeeping in City limits",
+#     body: "Per the city ordinance, you are allowed to keep 3 beehives within city limits.",
+#     image_url: "https://www.istockphoto.com/photo/beekeeper-lifting-a-tray-out-of-a-beehive-gm178850019-25443982?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fbeekeeping&utm_term=beekeeping%3A%3Asearch-aggressive-affiliates-v1%3Ab",
+#     user_id: 3,
+#     category_id: 3,
+#   },
+#   {
+#     title: "Edible Perennials",
+#     body: "Asparagus, chicory, dandelion, daylily",
+#     image_url: "https://www.istockphoto.com/photo/green-asparagus-sticks-gm1084123600-290865967?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fasparagus&utm_term=asparagus%3A%3Asearch-aggressive-affiliates-v1%3Ab",
+#     user_id: 4,
+#     category_id: 4,
+#   },
+# ])
+
+# Comment.create!([
+#   {
+#     body: "Asparagus is my favorite!",
+#     image_url: "https://unsplash.com/photos/YlXvFvfybao",
+#     user_id: 5,
+#     post_id: 4,
+#   },
+#   {
+#     body: "How do I harvest honey?",
+#     image_url: "https://unsplash.com/photos/YlXvFvfybao",
+#     user_id: 4,
+#     post_id: 3,
+#   },
+# ])
+
+# Meetup.create!([
+#   {
+#     title: "Planting Perennials in the Fall",
+#     location: "Jeffers Rd Gardens",
+#     image_url: "https://unsplash.com/photos/eL4ADAsiOR8",
+#     time: ""
+#   },
+# ])
